dce2f16f2c60c751515bce190ab6ea7d5a7c9236
anuskar12 <anuskar.2024151@nami.edu.np>
2025-02-12 08:08:19 +0545

    wow

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <SpeedInsights />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
